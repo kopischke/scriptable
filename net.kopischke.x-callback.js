423057702c1a9a6d4bf49d3c427f5d695a5fb01d
Martin Kopischke <martin@kopischke.net>
2020-11-03 12:58:42 +0100

    x-callback module 1.2.1
    Code and in-code documentation polish.

--- net.kopischke.x-callback.js
+++ net.kopischke.x-callback.js
@@ -5,47 +5,47 @@
  * Helpers for x-callback operations inside Scriptable.
  * Source master repository on {@link https://github.com/kopischke/scriptable|GitHub}).
  * @author Martin Kopischke <martin@kopischke.net>
- * @version 1.2.0
+ * @version 1.2.1
  * @license MIT
  * @module
  */
 
 /**
- * Handle x-callback returns, with fallbacks where no callback URLs are provided.
- *
- * This allows for vastly simplified returning of operation results to the caller
- * when a Scriptable script is executed via x-callback:
- * 
- * @example
- * const {XCallbackResult} = require('net.kopischke.x-callback.js')
- * const params = URLScheme.allParameters()
- * const result = new XCallbackResult(params)
- *
- * depending on outcome:
- * result.success({foo: 'bar'})
- * result.cancel({foo: 'bar'}, 'Dialog canceled by user')
- * result.error(err)
+ * x-callback URL handling function.
+ * @typedef {function} URLHandler
+ * @param {?object} payload - Key / value pairs to return as URL parameters.
+ */
+
+/**
  */
 module.exports.XCallbackResult = class XCallbackResult {
   /**
-   * Creates an XCallbackResult with handler functions for all canonical cases,
-   * independently of if appropriate callback URLs have been provided.
-   * @param {Object} parameters - An object with the x-callback return URLs.
-   * @param {String} [parameters.x-success] - The x-success base URL.
-   * @param {String} [parameters.x-cancel]  - The x-cancel base URL.
-   * @param {String} [parameters.x-error]   - The x-error base URL.
+   * Handle x-callback returns, with fallbacks where no callback URLs are provided.
+   *
+   * This allows for vastly simplified returning of operation results to the caller
+   * when a Scriptable script is executed via x-callback:
+   * 
+   * @example
+   * const {XCallbackResult} = require('net.kopischke.x-callback.js')
+   * const params = URLScheme.allParameters()
+   * const result = new XCallbackResult(params)
+   *
+   * depending on outcome:
+   * result.success({foo: 'bar'})
+   * result.cancel({foo: 'bar'}, 'Dialog canceled by user')
+   * result.error(err)
+   * @param {object} parameters - An object with the x-callback return URLs.
+   * @param {string} [parameters.x-success] - The x-success base URL.
+   * @param {string} [parameters.x-cancel]  - The x-cancel base URL.
+   * @param {string} [parameters.x-error]   - The x-error base URL.
+   * @property {URLHandler} successURL - The 'x-success' URL handler.
+   * @property {URLHandler} cancelURL - The 'x-cancel' URL handler.
+   * @property {URLHandler} errorURL - The 'x-error' URL handler.
    */
   constructor (parameters) {
     /**
-     * x-callback URL handling function.
-     * @typedef URLHandler
-     * @function
-     * @param {?Object} payload - Key / value pairs to return as URL parameters.
-     */
-
-    /**
      * Get an URL building function accepting variable payloads.
-     * @param   {?String} base - The base `x-(success|cancel|error)` callback URL.
+     * @param   {?string} base - The base `x-(success|cancel|error)` callback URL.
      * @returns {?URLHandler} null if no callback URL is provided.
      * @private
      */
@@ -67,18 +67,15 @@ module.exports.XCallbackResult = class XCallbackResult {
       }
     }
 
-    /** @type URLHandler */
     this.successURL = makeURLHandlerFor(parameters['x-success'])
-    /** @type URLHandler */
     this.cancelURL = makeURLHandlerFor(parameters['x-cancel'])
-    /** @type URLHandler */
     this.errorURL = makeURLHandlerFor(parameters['x-error'])
   }
-  
+
   /**
-   * x-success result handler.
-   * @param {?Object} payload - Key / value pairs to return as x-success URL parameters.
-   * @param {?String} message - Message to log if XCallbackResult#successURL is null.
+   * x-success result action.
+   * @param {?object} payload - Key / value pairs to return as x-success URL parameters.
+   * @param {?string} message - Message to log if XCallbackResult#successURL is null.
    */
   success (payload, message) {
     if (this.successURL != null) {
@@ -91,8 +88,8 @@ module.exports.XCallbackResult = class XCallbackResult {
   }
 
   /**
-   * x-cancel result handler.
-   * @param {?String} message - Message to log if XCallbackResult#cancelURL is null.
+   * x-cancel result action.
+   * @param {?string} message - Message to log if XCallbackResult#cancelURL is null.
    */
   cancel (message) {
     if (this.cancelURL != null) {
@@ -105,7 +102,7 @@ module.exports.XCallbackResult = class XCallbackResult {
   }
 
   /**
-   * x-error result handler.
+   * x-error result action.
    * @param {Error} err - The error triggering the x-error handler.
    * @throws The passed error if XCallbackResult#errorURL is null.
    */
